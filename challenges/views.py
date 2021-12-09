1641dc391b93637ae765f8a27aaa011c35522054
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-12-09 06:10:55 +0000

    Add new view and url

--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Index app')
+def january(request):
+    return HttpResponse('Eat not meat for the entire month!')
+
+def february(request):
+    return HttpResponse('Walk for at least 20 min every day!')
