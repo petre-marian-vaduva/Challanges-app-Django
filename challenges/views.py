47b613439ffaca79b6d1f35248057ed90407d240
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-12-10 04:05:10 +0000

    Dynamic path

--- challenges/views.py
+++ challenges/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def january(request):
-    return HttpResponse('Eat not meat for the entire month!')
+def monthly_challenges(request, month):
+    temp = None
+    if month == 'january':
+        temp = 'Eat not meat for the entire month!'
+    elif month == 'february':
+        temp = 'Walk for at least 20 min every day!'
+    else:
+        temp = 'Nothing'
+    return HttpResponse(temp)
 
-def february(request):
-    return HttpResponse('Walk for at least 20 min every day!')
